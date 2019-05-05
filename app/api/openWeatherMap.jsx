1c65f1f0037f516bc3f407255790127b29828c14
David Ramirez <drgorriz@gmail.com>
2019-05-05 17:44:38 +0200

    Fix error handle

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -8,14 +8,18 @@ module.exports = {
 		var encodedLocation = encodeURIComponent(location);
 		var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
-		return axios.get(requestUrl).then(function(response){
+		return axios.get(requestUrl, {
+		  validateStatus: function (status) {
+		    return status < 500;
+		  }
+		}).then(function(response){
 			if(response.data.cod && response.data.message){
 				throw new Error(response.data.message);
 			}else{
 				return response.data.main.temp;
 			}
-		},function(response){
-			throw new Error(response.data.message);
+		}).catch(function(response){
+			throw new Error('city not found');
 		});
 	}
 }
